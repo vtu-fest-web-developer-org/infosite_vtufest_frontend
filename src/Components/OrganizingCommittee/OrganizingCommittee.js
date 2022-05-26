dfe8c319e994b9a6741f748d7b8e17c4ec0b6d92
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2022-05-26 22:39:03 +0530

    Loader added to OC Page

--- src/Components/OrganizingCommittee/OrganizingCommittee.js
+++ src/Components/OrganizingCommittee/OrganizingCommittee.js
@@ -3,6 +3,7 @@ import { FiLink2 } from "react-icons/fi";
 import NavBar from "../NavBar/NavBar";
 import PageTitle from "../PageTitle/PageTitle";
 import colorDict from "../ColorDict/ColorDict";
+import LoaderSpinner from "../LoaderSpinner/LoaderSpinner";
 
 const OrganizingCommittee = (props) => {
   const [oc, setOc] = useState({});
@@ -19,64 +20,72 @@ const OrganizingCommittee = (props) => {
     <>
       <NavBar curPage="/organisingcommittee" />
       <div className="container">
-        <div className="row">
-          <PageTitle title="Organising Committee" />
-          {Object.keys(oc).map((dept) => {
-            return (
-              <div className="col-lg-6">
-                <div className={"alert " + colorDict[dept]} role="alert">
-                  {dept.toUpperCase()}
-                </div>
-                <div className="row justify-content-center">
-                  {oc[dept].map((person) => {
-                    return (
-                      <div className="col-lg-6">
-                        <div class="card text-center mb-3">
-                          <div class="card-body">
-                            <img
-                              className="profilephoto"
-                              src={person.photo}
-                              alt={person.name}
-                            />
-                            {person.name ? (
-                              <h5 class="card-title">{person.name}</h5>
-                            ) : (
-                              <br />
-                            )}
+        <PageTitle title="Organising Committee" />
+        {oc.developers ? (
+          <div className="row">
+            {Object.keys(oc).map((dept) => {
+              return (
+                <div className="col-lg-6">
+                  <div className={"alert " + colorDict[dept]} role="alert">
+                    {dept.toUpperCase()}
+                  </div>
+                  <div className="row justify-content-center">
+                    {oc[dept].map((person) => {
+                      return (
+                        <div className="col-lg-6">
+                          <div class="card text-center mb-3">
+                            <div class="card-body">
+                              <img
+                                className="profilephoto"
+                                src={person.photo}
+                                alt={person.name}
+                              />
+                              {person.name ? (
+                                <h5 class="card-title">{person.name}</h5>
+                              ) : (
+                                <br />
+                              )}
 
-                            {person.designation ? (
-                              <span class="card-text">
-                                {person.designation}
-                              </span>
-                            ) : (
-                              <br />
-                            )}
+                              {person.designation ? (
+                                <span class="card-text">
+                                  {person.designation}
+                                </span>
+                              ) : (
+                                <br />
+                              )}
 
-                            <br />
-                            {person.role ? <span>{person.role}</span> : <br />}
-                            <br />
-                            {person.link ? (
-                              <a
-                                href={person.link}
-                                class="text-decoration-none"
-                                rel="noreferrer"
-                                target={"_blank"}
-                              >
-                                <FiLink2 />
-                              </a>
-                            ) : (
                               <br />
-                            )}
+                              {person.role ? (
+                                <span>{person.role}</span>
+                              ) : (
+                                <br />
+                              )}
+                              <br />
+                              {person.link ? (
+                                <a
+                                  href={person.link}
+                                  class="text-decoration-none"
+                                  rel="noreferrer"
+                                  target={"_blank"}
+                                >
+                                  <FiLink2 />
+                                </a>
+                              ) : (
+                                <br />
+                              )}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    );
-                  })}
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        ) : (
+          <LoaderSpinner />
+        )}
       </div>
     </>
   );
