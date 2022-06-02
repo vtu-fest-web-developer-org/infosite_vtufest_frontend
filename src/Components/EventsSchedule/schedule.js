c638c8c985d41f4a74f4c916b81704999031dc09
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2022-06-02 23:35:17 +0530

    Coming soon added to schedule

--- src/Components/EventsSchedule/schedule.js
+++ src/Components/EventsSchedule/schedule.js
@@ -2,111 +2,111 @@ const schedule = {
   music: [
     {
       name: "Classical Vocal Solo (Hindustani/Karnataka)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Classical Instrumental Solo (Percussion Tal Vadya)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Light Vocal (India)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Classical Instrumental Solo (Non-Precussion Swar Vadya)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Western Vocal (Solo)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Group Song (Indian)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Group Song (Western)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Flok Orchestra",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
   ],
   dance: [
     {
       name: "Folk / Tribal Dance",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Classical Dance (Indian)",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
   ],
   literary: [
     {
       name: "Quiz",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Elocution",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Debate",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
   ],
   theatre: [
     {
       name: "One Act Play",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Skit",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Mime",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Mimicry",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
   ],
   "fine-arts": [
     {
       name: "Collage",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Installation",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "On the Spot Painting",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Poster Making",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Clay Modelling",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Cartooning",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Rangoli",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
     {
       name: "Spot Photography",
-      details: "Anything can be written here",
+      details: "Coming Soon",
     },
   ],
 };
