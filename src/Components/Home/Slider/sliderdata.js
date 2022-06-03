c2252bc9dcaa4123c3c06fd93b6c8c6b10707080
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2022-06-03 10:22:48 +0530

    Slider updates

--- src/Components/Home/Slider/sliderdata.js
+++ src/Components/Home/Slider/sliderdata.js
@@ -1,5 +1,9 @@
 const sliderdata = {
-  active: [{ photu: "/assets/slider/banner1.png" }],
-  inactive: [{ photu: "" }, { photu: "" }],
+  active: [{ photu: "/assets/slider/Artboard 1.png" }],
+  inactive: [
+    { photu: "/assets/slider/Artboard 2.png" },
+    { photu: "/assets/slider/Artboard 3.png" },
+    { photu: "/assets/slider/Artboard 4.png" },
+  ],
 };
 export default sliderdata;
