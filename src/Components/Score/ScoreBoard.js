f394a95e2f68c889d507154114e1696b9673243e
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2022-07-29 08:40:53 +0530

    score added with sheets

--- src/Components/Score/ScoreBoard.js
+++ src/Components/Score/ScoreBoard.js
@@ -7,46 +7,10 @@ const ScoreBoard = () => {
       <div className="container mt-2">
         <div className="row">
           <div className="col-12">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">Event Name</th>
-                  <th scope="col">
-                    First
-                    <br />
-                    College (Score)
-                  </th>
-                  <th scope="col">
-                    Second
-                    <br />
-                    College (Score)
-                  </th>
-                  <th scope="col">
-                    Third
-                    <br />
-                    College (Score)
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {score.map((sco) => {
-                  return (
-                    <tr>
-                      <th scope="row">{sco.name}</th>
-                      <td>
-                        {sco.first.collegeId} ( {sco.first.points} )
-                      </td>
-                      <td>
-                        {sco.second.collegeId} ( {sco.second.points} )
-                      </td>
-                      <td>
-                        {sco.third.collegeId} ( {sco.third.points} )
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <iframe
+              className="score"
+              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTYgMrPbhrnms5pKRns7os9MSQPQjz0deLByDNhfbf7THwcvsEFcF80khFZ2UAgu2UQBopKMjiVYNeo/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
+            ></iframe>
           </div>
         </div>
       </div>
